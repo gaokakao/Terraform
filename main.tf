16c8ec7d95ed40f81567905d59d5eb7c96458dd5
gaokakao <giedriuskk@gmail.com>
2022-03-04 14:52:35 +0000

    save

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
-# Provider
  provider "aws" {
+
    access_key = "AKIATMKUVKWYISIFEKW5"
    secret_key = "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"
    region = "eu-west-1"
- } 1
+ } 
+
+
+# ami-08ca3fed11864d6bb
+
+resource "aws_instance" "mano" {
+ami = "ami-08ca3fed11864d6bb"
+instance_type = "t2.micro"
+  
+}

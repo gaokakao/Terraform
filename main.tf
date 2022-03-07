c87496c44332b579d0473a63eee79551a2964ed2
gaokakao <giedriuskk@gmail.com>
2022-03-07 14:53:36 +0000

    EC2 ir RDS sukuria

--- main.tf
+++ main.tf
@@ -13,17 +13,22 @@ ami = "ami-08ca3fed11864d6bb"
 instance_type = "t2.micro"
 
   tags = {
-    Name = "Pirmas"
+    Name = "Bandomasis Instance"
   }  
 
 
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-08ca3fed11864d6bb"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "Mano Instance"
-  }
+resource "aws_db_instance" "Incogni" {
+allocated_storage = 10
+identifier = "incogni"
+storage_type = "gp2"
+engine = "mysql"
+engine_version = "5.7"
+instance_class = "db.t2.large"
+db_name = "incogni"
+username = "root"
+password = "down1234"
+parameter_group_name = "default.mysql5.7"
 }
